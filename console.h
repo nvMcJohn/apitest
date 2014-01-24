35d84eab093c81851321da0d71e949b63b1f6f3d
nvMcJohn <jmcdonald@nvidia.com>
2014-01-24 17:07:29 -0600

    Removed dead files that have been relocated

--- console.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace console
-{
-    void debug(const char* fmt, ...);
-    void error(const char* fmt, ...);
-}
--- streaming_vb_gl_fs.glsl
+++
@@ -1,8 +0,0 @@
-#version 430
-
-layout (location = 0) out vec4 FS_Col;
-
-void main()
-{
-	FS_Col = vec4(1, 1, 1, 1);
-}
