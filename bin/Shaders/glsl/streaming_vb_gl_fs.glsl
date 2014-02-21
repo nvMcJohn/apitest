df5aa3b06b74a3247f01ddfc2625d8f6b07bc13b
nvMcJohn <jmcdonald@nvidia.com>
2014-02-21 14:09:36 -0600

    Remove dead files, relocate shaders for streaming vb GL stuff in solution. Update streaming vb shaders to use version 420 instead of 430. Fix minor commenting issue in bindless_multidraw_fs.glsl

--- bin/Shaders/glsl/streaming_vb_gl_fs.glsl
+++ bin/Shaders/glsl/streaming_vb_gl_fs.glsl
@@ -1,7 +1,15 @@
-#version 430
+// NOTE: Unlike every other set of Solutions, all of the StreamingVBs currently use this shader.
+// If you need to make modifications for a particular test, create a new shader. 
+#version 420
 
-layout (location = 0) out vec4 FS_Col;
+// Uniforms / SSBO ----------------------------------------------------------------------------------------------------
 
+// Input --------------------------------------------------------------------------------------------------------------
+
+// Output -------------------------------------------------------------------------------------------------------------
+layout(location = 0) out vec4 FS_Col;
+
+// Functions ----------------------------------------------------------------------------------------------------------
 void main()
 {
 	FS_Col = vec4(1, 1, 1, 1);
--- bin/Shaders/glsl/streaming_vb_gl_vs.glsl
+++ bin/Shaders/glsl/streaming_vb_gl_vs.glsl
@@ -1,13 +1,20 @@
-#version 430
+// NOTE: Unlike every other set of Solutions, all of the StreamingVBs currently use this shader.
+// If you need to make modifications for a particular test, create a new shader. 
+#version 420
 
-layout(std140, binding = 0) uniform CB0
+// Uniforms / SSBO ----------------------------------------------------------------------------------------------------
+layout(std140) uniform CB0
 {
     vec2 Viewport;
 };
 
-layout (location = 0) in vec2 Attr_Pos;
+// Input --------------------------------------------------------------------------------------------------------------
+layout(location = 0) in vec2 In_v2Pos;
 
+// Output -------------------------------------------------------------------------------------------------------------
+
+// Functions ----------------------------------------------------------------------------------------------------------
 void main()
 {
-    gl_Position = vec4(Attr_Pos * Viewport + vec2(-1, 1), 0, 1);
+    gl_Position = vec4(In_v2Pos * Viewport + vec2(-1, 1), 0, 1);
 }
