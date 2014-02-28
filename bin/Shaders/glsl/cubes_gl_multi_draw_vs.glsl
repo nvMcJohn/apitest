eea04fd0ce5f5d8700a367e98943f33efe6fac2c
nvMcJohn <jmcdonald@nvidia.com>
2014-02-28 16:52:57 -0600

    Missed changes to support side-by-side usage of DrawIDARB and not.

--- bin/Shaders/glsl/cubes_gl_multi_draw_vs.glsl
+++ bin/Shaders/glsl/cubes_gl_multi_draw_vs.glsl
@@ -1,8 +1,9 @@
 #version 420
-#define USE_ARB_DRAWID 1
-#if USE_ARB_DRAWID
-#extension GL_ARB_shader_draw_parameters : require
+
+#ifdef USE_SHADER_DRAW_PARAMETERS
+#	extension GL_ARB_shader_draw_parameters : require
 #endif
+
 #extension GL_ARB_shader_storage_buffer_object : require
 
 // Uniforms / SSBO ----------------------------------------------------------------------------------------------------
@@ -16,10 +17,14 @@ uniform mat4 ViewProjection;
 // Input --------------------------------------------------------------------------------------------------------------
 layout(location = 0) in vec3 In_v3Pos;
 layout(location = 1) in vec3 In_v3Color;
-#if USE_ARB_DRAWID
-in int gl_DrawIDARB;
-#else
-layout(location = 2) in int In_DrawID;
+#ifndef USE_SHADER_DRAW_PARAMETERS
+	layout(location = 2) in int In_iDrawID;
+#endif
+
+#ifdef USE_SHADER_DRAW_PARAMETERS
+#	define DrawID gl_DrawIDARB
+#else 
+#	define DrawID In_iDrawID
 #endif
 
 // Output -------------------------------------------------------------------------------------------------------------
@@ -30,12 +35,7 @@ out block {
 // Functions ----------------------------------------------------------------------------------------------------------
 void main()
 {
-#if USE_ARB_DRAWID
-    int drawID = gl_DrawIDARB;
-#else
-    int drawID = In_DrawID;
-#endif
-    mat4 World = Transforms[drawID];
+    mat4 World = Transforms[DrawID];
     vec3 worldPos = vec3(World * vec4(In_v3Pos, 1));
     gl_Position = ViewProjection * vec4(worldPos, 1);
     Out.v3Color = In_v3Color;
